e51fad8f6fd3fd6c0a6a57bf7755f4a9f82ead82
AaYang0312 <liu20031221@outlook.com>
2026-02-21 20:11:24 +0800

    update

--- gomoddemo1/main.go
+++ gomoddemo1/main.go
@@ -1,4 +1,13 @@
 package main
 
+import (
+	"fmt"
+	"gomoddemo1/calc"
+)
+
 func main() {
+	sum := clac.Add(1, 2)
+	fmt.Println(sum)
+	//fmt.Println(calc.aaa) // 首字母小写无法访问
+	fmt.Println(calc.AAA)
 }
