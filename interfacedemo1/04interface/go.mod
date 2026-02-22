cc929981de6142a0608af84c1b0c27e3d3a95ba3
AaYang0312 <liu20031221@outlook.com>
2026-02-22 20:26:17 +0800

    Update 2.22

---
+++ interfacedemo1/04interface/go.mod
@@ -0,0 +1,3 @@
+module 04interface
+
+go 1.25.0
---
+++ interfacedemo1/04interface/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+type Usber interface {
+	start()
+	stop()
+}
+
+type Phone struct {
+	Name string
+}
+
+func (p Phone) start() {
+	fmt.Printf("%v启动\n", p.Name)
+}
+func (p Phone) stop() {
+	fmt.Printf("%v关闭\n", p.Name)
+}
+
+func main() {
+	var p1 = Phone{
+		"小米手机",
+	}
+	var p2 Usber = p1
+	p2.start()
+
+	var p3 = &Phone{
+		"苹果手机",
+	}
+	var p4 Usber = p3
+	p4.start()
+}
