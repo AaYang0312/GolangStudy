34f7a0012666e0466302807a86845c4df3385cc5
AaYang0312 <liu20031221@outlook.com>
2026-02-23 13:14:38 +0800

    Update 2.23

--- interfacedemo1/04interface/main.go
+++ interfacedemo1/04interface/main.go
@@ -11,19 +11,19 @@ type Phone struct {
 	Name string
 }
 
-func (p Phone) start() {
+func (p *Phone) start() { // 指针类型接收者，如果是指针类型接收者必须用下面方法实现接口
 	fmt.Printf("%v启动\n", p.Name)
 }
-func (p Phone) stop() {
+func (p *Phone) stop() {
 	fmt.Printf("%v关闭\n", p.Name)
 }
 
 func main() {
-	var p1 = Phone{
-		"小米手机",
-	}
-	var p2 Usber = p1
-	p2.start()
+	// var p1 = Phone{
+	// 	"小米手机",
+	// }
+	// var p2 Usber = p1
+	// p2.start()
 
 	var p3 = &Phone{
 		"苹果手机",
