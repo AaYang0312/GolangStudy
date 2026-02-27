072004b2ec837dc021006058ead606d1b2176fc5
AaYang0312 <liu20031221@outlook.com>
2026-02-28 01:31:22 +0800

    Update 2.28

---
+++ IO/IO01/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	// 1.读取文件
+	file, err := os.Open("./main.go")
+	defer file.Close()
+
+	if err != nil {
+		fmt.Println("出错:", err)
+		return
+	}
+
+	// 2.读取内容
+	var strSlice []byte
+	var tempSlice = make([]byte, 128)
+	for {
+		n, err := file.Read(tempSlice)
+		if err == io.EOF {
+			fmt.Println("读取完毕")
+			break
+		}
+		if err != nil {
+			fmt.Println("读取失败")
+			return
+		}
+		// fmt.Printf("读取到了%d个字节\n", n)
+		strSlice = append(strSlice, tempSlice[:n]...) // 注意写法，避免temp最后未被完全覆盖
+	}
+	fmt.Printf("读取到内容为: %v\n", string(strSlice))
+
+}
