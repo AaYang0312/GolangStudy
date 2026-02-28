5e62d8b6d5493f7c785b975d73d5a7cf73523c16
AaYang0312 <liu20031221@outlook.com>
2026-02-28 22:28:53 +0800

    Update 2.28

---
+++ gindemo/gindemo01/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// 创建一个默认的路由引擎
+	r := gin.Default()
+	// 配置路由
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{ // c.JSON: 返回 JSON 格式的数据
+			"message": "Hello World",
+		})
+	})
+	// 启动 HTTP 服务，默认在 0.0.0.0:8080
+}
